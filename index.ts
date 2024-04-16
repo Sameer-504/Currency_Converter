6d651b9146e5b72706ad7c7a52ea7a4ae0c365b1
Sameer Anis <159227804+Sameer-504@users.noreply.github.com>
2024-04-16 16:47:23 +0500

    Currency Converter App

---
+++ index.ts
@@ -0,0 +1,43 @@
+import inquirer from 'inquirer'
+
+
+const Currency: any = {
+    USD: 1, //Base Currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+
+
+let user_asnwer = await inquirer.prompt(
+    [
+        {
+            name: 'from',
+            message: "Enter from Currency",
+            type: 'list',
+            choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+        },
+        {
+            name: 'to',
+            message: "Enter to Currency",
+            type: 'list',
+            choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+        },
+        {
+            name: 'amount',
+            message: "Enter Your Amount",
+            type: 'number'
+        }
+    ]
+)
+
+
+let fromAmount = Currency [user_asnwer.from]
+let toAmount = Currency [user_asnwer.to]
+let amount = user_asnwer.amount
+let baseAmount = amount / fromAmount   // USD Base Currency
+let covertedAmount = baseAmount * toAmount
+            //    15000  / 280
+
+ console.log(covertedAmount);
